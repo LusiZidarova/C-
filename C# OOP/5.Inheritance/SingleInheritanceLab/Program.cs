8fcb7d4055fcfb8208b3e7bb36987d4033686bce
LusiZidarova <75747610+LusiZidarova@users.noreply.github.com>
2021-01-04 12:01:09 +0200

    Add files via upload

---
+++ C# OOP/5.Inheritance/SingleInheritanceLab/Program.cs
@@ -0,0 +1,16 @@
+﻿using Farm;
+using System;
+
+namespace _1.SingleInheritanceLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+
+            dog.Eat();
+            dog.Bark();
+        }
+    }
+}
