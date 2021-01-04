280707fe0b895cae5bb3d6248cdf8da4c3f19de7
LusiZidarova <75747610+LusiZidarova@users.noreply.github.com>
2021-01-04 11:44:11 +0200

    Add files via upload

---
+++ C# OOP/3.Abstraction/TelephonyEx/IBrowsable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TelephonyEx
+{
+   public  interface IBrowsable
+    {
+        void Browse(string url);
+    }
+}
---
+++ C# OOP/3.Abstraction/TelephonyEx/Smartfone.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TelephonyEx
+{
+    public class Smartfone : ICallable, IBrowsable
+    {
+        public void Browse(string url)
+        {
+            Console.WriteLine($"Browsing... {url}");
+        }
+
+        public void Call(string number)
+        {
+            Console.WriteLine($"Calling... {number}");
+        }
+    }
+}
